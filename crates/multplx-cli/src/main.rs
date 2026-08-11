c771fad019ef5753c34042a8fe115f30e89ec423
Kashyap Tanuku <kashyapt.business@gmail.com>
2026-08-10 23:41:37 -0400

    Flush Rust CLI output before exit

--- crates/multplx-cli/src/main.rs
+++ crates/multplx-cli/src/main.rs
@@ -1,5 +1,6 @@
 use multplx_cli::Cli;
+use std::process::ExitCode;
 
-fn main() {
-    std::process::exit(Cli::parse_multicall(std::env::args_os()).run());
+fn main() -> ExitCode {
+    ExitCode::from(Cli::parse_multicall(std::env::args_os()).run() as u8)
 }
